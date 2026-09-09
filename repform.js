3c7bc472483265c1a954a8c0459b96e2eeaf478e
repfive-active <d_madari@hotmail.com>
2026-09-09 16:49:32 -0700

    Log challenge status and object for debugging

--- repform.js
+++ repform.js
@@ -143,8 +143,12 @@ async function loadRep() {
 
     repData = data;
     // CHALLENGE STATUS
-  
+    
     const challengeStatus = (data.challenge["Status"] || "").trim();
+
+    console.log("Challenge Status:", challengeStatus);
+    console.log("Challenge Object:", data.challenge);
+    
     if (challengeStatus !== "Active") {
     throw new Error(
       "This challenge is not Active."
